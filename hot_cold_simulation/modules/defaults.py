a91c5dc0b04eea86b217c014084fb24c8b2e45ee
Seth Docherty <sddocherty@gmail.com>
2023-10-20 12:16:03 -0400

    Migrated database configuration variables
    All modifiable project variables and their corresponding default values are populated in the defaults.py file.  Running the generate-config script creates all the .env files which can be modified by the user.

--- hot_cold_simulation/modules/defaults.py
+++ hot_cold_simulation/modules/defaults.py
@@ -1,22 +1,11 @@
 # default_config.py
 
 default_configurations = {
-    "main": {
-        "VAR1": "default_value1",
-        "VAR2": "default_value2",
-        "VAR3": "default_value3",
-        "VAR4": "",
-        "VAR5": "",
-        "VAR6": 12345.5,
-        "VAR7": 12345.5,
-    },
-    "other": {
-        "VAR1": "default_value1",
-        "VAR2": "default_value2",
-        "cc": "",
-        "VAR3": "default_value3",
-        "VAR4": "",
-        "VAR5": "",
-        "VAR6": 12345.5,
+    "database": {
+        "DB_NAME": "railway",
+        "DB_USER": "postgres",
+        "DB_PASS": "",
+        "DB_HOST": "",
+        "DB_PORT": "6563",
     },
 }
